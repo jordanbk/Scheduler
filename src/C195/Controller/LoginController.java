eb13d4ec2b72d9508ab51b6978e86c8dfc39e13e
JordanBK <jburke0513@gmail.com>
2021-10-16 11:15:50 -0500

    10.16

--- src/C195/Controller/LoginController.java
+++ src/C195/Controller/LoginController.java
@@ -2,6 +2,7 @@ package C195.Controller;
 
 import C195.Database.DatabaseConnection;
 import C195.Model.User;
+import C195.Utils.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -19,6 +20,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -48,7 +50,8 @@ public class LoginController implements Initializable {
         String username = usernameTxt.getText();
         String password = passwordTxt.getText();
         loginMessageLabel.setText("You tried to login");
-        //int userID = getUserID(username);
+        boolean login = LoginController.validateLogin(username, password);
+        Logger.AuditLogger(username, login);
 
         if (!validateLogin(username, password)) {
             Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -56,8 +59,8 @@ public class LoginController implements Initializable {
             alert.setHeaderText("Incorrect Username and/or Password");
             alert.setContentText("Enter valid Username and Password");
             Optional<ButtonType> result = alert.showAndWait();
+
         } else {
-        //int UserID = getUserID(username);
 
             root = FXMLLoader.load(getClass().getResource("../Views/MainMenu.fxml"));
             stage = (Stage) usernameTxt.getScene().getWindow();
@@ -68,14 +71,13 @@ public class LoginController implements Initializable {
         }
     }
 
-    public boolean validateLogin(String username, String password) throws SQLException {
+    public static boolean validateLogin(String username, String password) throws SQLException {
 
         try{
             DatabaseConnection.openConnection();
             String searchStatement = "SELECT * FROM users WHERE User_Name=? AND Password=?";
             PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement(searchStatement);
 
-            //PreparedStatement pst = DatabaseConnection.conn.prepareStatement("SELECT * FROM users WHERE User_Name=? AND Password=?");
             ps.setString(1, username);
             ps.setString(2, password);
             ResultSet rs = ps.executeQuery();
@@ -93,21 +95,5 @@ public class LoginController implements Initializable {
         }
         return false;
     }
-
-
- /*   private int getUserID(String username) throws SQLException {
-        int userID = -1;
-
-        Statement statement = DatabaseConnection.conn.createStatement();
-
-        String sqlStatement = "SELECT User_ID FROM users WHERE User_Name ='" + username + "'";
-
-        ResultSet result = statement.executeQuery(sqlStatement);
-
-        while (result.next()) {
-            userID = result.getInt("User_ID");
-        }
-        return userID;
-    }*/
 
 }
--- src/C195/Utils/Logger.java
+++ src/C195/Utils/Logger.java
@@ -3,7 +3,23 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-public class Logger {
 
+public class Logger {
+    private static final String FILENAME = "login_activity.txt";
+
+    public Logger() {}
+
+    public static void AuditLogger (String username, boolean success) {
+        try (FileWriter filewriter = new FileWriter(FILENAME, true);
+             BufferedWriter bufferedwriter = new BufferedWriter(filewriter);
+             PrintWriter printwriter = new PrintWriter(bufferedwriter)) {
+             bufferedwriter.append(LocalDateTime.now() + "," + " Username: " + username + "," +
+                    " Login Success: " + (success ? " Success" : " Failure") + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
