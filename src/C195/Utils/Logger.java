678bd8c1cc2987843e8e675a85121b537d885970
JordanBK <jburke0513@gmail.com>
2021-10-16 09:59:37 -0500

    Create Logger.java

---
+++ src/C195/Utils/Logger.java
@@ -0,0 +1,9 @@
+package C195.Utils;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.ZonedDateTime;
+public class Logger {
+
+}
